b1b17d8c02627283420e9bcac198f1c63cbbabca
Tan <htan@ndc.nasa.gov>
2018-05-18 15:50:11 -0700

    add css and more changes

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,14 +1,17 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 import 'src/hero.dart';
+import 'src/mock_heroes.dart';
 
 @Component(
   selector: 'my-app',
-  directives: [formDirectives],
+  directives: [coreDirectives, formDirectives],
   templateUrl: 'app_component.html',
+  styleUrls: ['app_component.css'],
 )
 
 class AppComponent {
   final title = 'Tour of Heroes';
+  List<Hero> heroes = mockHeroes;
   Hero hero = new Hero(1,'Windstorm');
 }
